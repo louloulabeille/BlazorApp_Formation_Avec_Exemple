3c579db9780295975f219c1c47ba4706d435d680
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-04-21 13:24:05 +0200

    Nombre magique

---
+++ BlazorApp_Formation_Avec_Exemple/Components/Pages/Counter.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_Formation_Avec_Exemple.Components.Pages
+{
+    /// <summary>
+    /// plusieur facon de dissocié la vue avec le code, ici on a une classe de base pour le code et un fichier .razor pour la vue
+    /// ou créer un classe partiel qui prend le meme nom que la classe razor. ex : 
+    /// Counter.razor.cs et Counter.razor 
+    /// public partial class Counter
+    /// 
+    /// sinon il y a le MVVM
+    /// </summary>
+    public class CounterBase: ComponentBase
+    {
+        protected int currentCount = 0;
+
+        [Parameter]
+        public int InitialCount { get => currentCount; set => currentCount = value; }
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ BlazorApp_Formation_Avec_Exemple/Components/Pages/NombreMagique.razor.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_Formation_Avec_Exemple.Components.Pages
+{
+    public class NombreMagiqueBase : ComponentBase
+    {
+        #region protected properties
+        protected int EntryNbMagique { get; set; }  = 0;
+        protected List<string> Life = [];
+        protected bool IsGameOver => Life.Count == 0;
+        protected bool IsGameWin => EntryNbMagique == NbMagique;
+        protected int NbMagique;
+        #endregion
+
+        #region private properties
+        private int _nbLifeDefault = 5;
+        #endregion
+
+        #region constructeur
+        public NombreMagiqueBase() {
+            NbMagique = GenereMagicNumber();
+            InitNbLife();
+        }
+        #endregion
+
+        #region protected method
+
+        /// <summary>
+        /// method qui teste le nombre magique , si le nombre est incorrecte on retire une vie
+        /// </summary>
+        protected void TesterNbMagique()
+        {
+            try
+            {
+                if (EntryNbMagique <= 0 || EntryNbMagique > 20) {
+                    EntryNbMagique = 0;
+                    return;
+                } 
+                if (EntryNbMagique == NbMagique) { return; }
+                Life.RemoveAt(Life.Count - 1);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// re-initalise leu magiques 
+        /// </summary>
+        protected void ReinitialiserJeu()
+        {
+            NbMagique = GenereMagicNumber();
+            InitNbLife();
+        }
+
+        #endregion
+
+
+        #region private method 
+        /// <summary>
+        /// méthode qui retourne un nombre random entre 1 & 20
+        /// </summary>
+        /// <returns></returns>
+        private static int GenereMagicNumber()
+        {
+            Random random = new ();
+            return random.Next(1, 21); // Génère un nombre aléatoire entre 1 et 50
+        }
+
+        /// <summary>
+        /// initialise le nombre de vie pour le jeux Nombre magique
+        /// </summary>
+        private void InitNbLife()
+        {
+            Life.Clear();
+            for (int i = 0; i < _nbLifeDefault; i++)
+            {
+                Life.Add("lib/img/icons8-red-heart-48.png");
+            }
+        }
+
+        #endregion
+    }
+}
