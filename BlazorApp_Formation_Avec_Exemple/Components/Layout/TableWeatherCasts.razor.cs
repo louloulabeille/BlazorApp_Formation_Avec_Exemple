731294102efb79a5cb4537594928fd7af004cdbf
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-04-30 16:35:14 +0200

    bind two way parent enfant - putain le code donnée dans le cours ne marche pas

---
+++ BlazorApp_Formation_Avec_Exemple/Components/Layout/TableWeatherCasts.razor.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp_Formation_Avec_Exemple.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_Formation_Avec_Exemple.Components.Layout
+{
+    public class TableWeatherCastsBase : ComponentBase
+    {
+        /// <summary>
+        ///  pour binder une propriété il faut créer 
+        /// 2 parameters un sur la propertie et l'autre sur un delegate Action ave le nom de la propertie + Changed
+        /// @bind-Forecasts
+        /// </summary>
+
+        #region public properties
+        [Parameter]
+        public WeatherForecast[]? Forecasts { get; set; }
+        #endregion
+
+        #region public properties parameter
+        //public IEnumerable<WeatherForecast>? Forecasts { get; set; }
+        [Parameter]
+        public EventCallback<WeatherForecast[]?> ForecastsChanged { get; set; }
+        #endregion
+
+        #region protected method view
+        protected void Clear()
+        {
+            Forecasts = null;
+            ForecastsChanged.InvokeAsync(Forecasts); // - method qui repasse la data vers le parent
+        }
+        #endregion
+
+    }
+}
--- BlazorApp_Formation_Avec_Exemple/Components/Pages/Weather.razor.cs
+++ BlazorApp_Formation_Avec_Exemple/Components/Pages/Weather.razor.cs
@@ -1,18 +1,27 @@
 ﻿using BlazorApp_Formation_Avec_Exemple.Interfaces.Services;
 using BlazorApp_Formation_Avec_Exemple.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorApp_Formation_Avec_Exemple.Components.Pages
 {
     public class WeatherBase : ComponentBase
     {
+        #region private properties
+        //private WeatherForecast[]? _forecasts;
+        #endregion
+
         #region injection de dépendance par propriété 
         [Inject]
         public IWheatherForecastService? WheatherForecastService { get; set; }
         #endregion
 
         #region protected properties view
-        protected WeatherForecast[]? Forecasts { get; set; }
+        /*protected WeatherForecast[]? Forecasts { get { return _forecasts; } set {
+                _forecasts = value;
+                Console.WriteLine($"il est bien mis à jour {_forecasts?.Count()}");
+            } }*/
+        protected WeatherForecast[]? Forecasts;
 
         protected DateTime DateSaisie { get; set; } = DateTime.Today;
         #endregion
@@ -46,16 +55,15 @@ namespace BlazorApp_Formation_Avec_Exemple.Components.Pages
         private async Task InitializeForecasts()
         {
             // -- appel de la class par injection pour retourner les datas
-            _ = Task.Delay(5000).ContinueWith(async _ =>
+            await Task.Delay(5000);
+            if (WheatherForecastService is not null)
             {
-                if (WheatherForecastService is not null)
-                {
-                    Forecasts = await WheatherForecastService.GetForecastAsync(DateSaisie);
-                    // - rafraichi la page
-                    await this.InvokeAsync(StateHasChanged);
-                }
+                Forecasts = await WheatherForecastService.GetForecastAsync(DateSaisie);
+                // - rafraichi la page
+                //await this.InvokeAsync(StateHasChanged);
+            }
                     
-            });
+            
 
         }
         #endregion
